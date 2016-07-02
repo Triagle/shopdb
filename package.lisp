c215b441ced815eebca85b8caac3a514ab19db28
Jake Faulkner <jakefaulkn@gmail.com>
2016-07-02 13:07:53 +1200

    Update package file

--- package.lisp
+++ package.lisp
@@ -1,11 +1,27 @@
 ;;;; package.lisp
-
-(defpackage #:shopdb
-  (:use #:cl #:hunchentoot #:cl-who))
 (defpackage #:resources
-  (:use #:cl #:sqlite))
+  (:use #:cl #:sqlite)
+  (:export *db*))
+(defpackage :sxql.composer
+  (:use :cl)
+  (:export :and-where
+   :or-where
+           :where=
+   :from=
+           :from+
+   :fields=
+           :fields+
+   :limit=
+           :offset=
+   :order-by=
+           :group-by=
+   :order-by+
+           :group-by+
+   :returning=
+           :having=)
+  (:documentation "Dynamic SXQL query composition"))
 (defpackage #:database
-  (:use #:cl #:resources #:sqlite #:cl-ppcre))
-(defpackage #:database-test
-  (:use #:cl #:prove  #:database #:sqlite)
-  )
+  (:use #:cl #:resources #:sqlite #:sxql #:sxql.composer #:cl-ppcre)
+  (:export parse-search run-db-search get-product-for))
+(defpackage #:shopdb
+  (:use #:cl #:hunchentoot #:cl-who #:database))
