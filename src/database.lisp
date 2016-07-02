0fe863d28feb6cf26c3c0b9dc85543f528cf1574
Jake Faulkner <jakefaulkn@gmail.com>
2016-07-02 13:01:06 +1200

    Add parse-search test & function

--- src/database.lisp
+++ src/database.lisp
@@ -1,5 +1,14 @@
 (in-package #:database)
+(defun category? (string)
+  "Returns t if string is a category, nil otherwise"
+  (not (null (scan "(?:\\+\\w+)" string)))) ;; check that the string matches the following pattern +[word] where word denotes the catagory
+(defun separate (string)
+  (split " " string))
+(defun string-cdr (string)
+  (subseq string 1))
 (defun parse-search (search)
   "Parse search into constituents"
-
-  )
+  (let* ((tokens (separate search))
+         (categories (mapcar #'string-cdr (remove-if-not #'category? tokens)))
+         (search-terms (remove-if #'category? tokens)))
+    (list :search search-terms :categories categories)))
--- test/database.lisp
+++ test/database.lisp
@@ -1,17 +1,10 @@
 (in-package #:database-test)
 
-(plan 6)
-
-;;; perform-shopdb-query test
-;; Expected behaviour: make an SQL query and executes it
-(is (perform-sql-query "select name from shop where name like '%test%'") '("test"))
-(is (perform-sql-query "some broken sql query") 'malformed-query)
-;; end test
+(plan 2)
 
 ;;; parse-search test
 ;; Expected behaviour: parse a query into it's keyword and catagories
-(is (parse-search "intel +cpu") '(:search-term "intel" :catagories "cpu"))
-(is (parse-search "intel + cpu") 'malformed-search)
+(is (parse-search "intel +cpu") '(:search ("intel") :categories ("cpu")))
 ;; end test
 
 ;;; construct-db-search test
